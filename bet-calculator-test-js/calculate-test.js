5ec94ec3916e3edf1c6bb20df8b387794119e260
Mileta Cekovic <mcekovic@gmail.com>
2021-01-15 14:07:38 +0100

    More JS tests

--- bet-calculator-test-js/calculate-test.js
+++ bet-calculator-test-js/calculate-test.js
@@ -1,11 +1,31 @@
 const kotlin = require('../bet-calculator/build/js/node_modules/kotlin/kotlin.js').kotlin;
 const bc = require('../bet-calculator/build/js/node_modules/bet-calculator/kotlin/bet-calculator.js').org.strangeforest.betcalculator;
 
-const bet = bc.Bet_init(bc.bettypes.Single, '10', new kotlin.collections.ArrayList([
+// Calculate Single Bet test
+
+const bet1 = bc.Bet_init(bc.bettypes.Single, '10', new kotlin.collections.ArrayList([
 	bc.BetLeg_init('2.0')
 ]));
 
-const result = bc.BetCaptureCalculator.calculate(bet);
+console.log(bc.BetCaptureCalculator.calculate(bet1));
 
-console.log(result);
 
+// Calculate Accumulator Bet test
+
+const bet2 = bc.Bet_init(bc.bettypes.Accumulator, '10', new kotlin.collections.ArrayList([
+	bc.BetLeg_init('2.0'),
+	bc.BetLeg_init('3.0')
+]));
+
+console.log(bc.BetCaptureCalculator.calculate(bet2));
+
+
+// Calculate Doubles Bet test
+
+const bet3 = bc.Bet_init(bc.bettypes.Doubles, '10', new kotlin.collections.ArrayList([
+	bc.BetLeg_init('2.0'),
+	bc.BetLeg_init('3.0'),
+	bc.BetLeg_init('4.0')
+]));
+
+console.log(bc.BetCaptureCalculator.calculate(bet3));
