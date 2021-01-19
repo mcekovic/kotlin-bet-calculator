639a2c71bb7ef81b681a571e5676426b173d358b
Mileta Cekovic <mcekovic@gmail.com>
2021-01-19 19:31:33 +0100

    Multiplatform API

--- bet-calculator-test-jvm/src/test/java/org/strangeforest/betcalculator/test/BetCaptureCalculationTest.java
+++ bet-calculator-test-jvm/src/test/java/org/strangeforest/betcalculator/test/BetCaptureCalculationTest.java
@@ -1,81 +1,76 @@
 package org.strangeforest.betcalculator.test;
 
-import java.util.*;
-
 import org.junit.jupiter.api.*;
 import org.strangeforest.betcalculator.*;
-import org.strangeforest.betcalculator.bettypes.*;
-import org.strangeforest.betcalculator.interrelation.*;
-import org.strangeforest.betcalculator.rules.*;
-import org.strangeforest.betcalculator.util.*;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.strangeforest.betcalculator.BetCalculatorKt.*;
 
 class BetCaptureCalculationTest {
 
 	@Test
 	void calculateSingleBet() {
-		var bet = new Bet(Single.INSTANCE, "10", List.of(
-			new BetLeg("2.0", LegStatus.Companion.getOPEN(), IRDescriptor.Companion.getNO_IR(), false)
-		), List.of(), BetRules.Companion.getDEFAULT());
+		var bet = new Bet("Single", "10", new BetLeg[] {
+			new BetLeg("2.0")
+		});
 
-		var result = BetCaptureCalculator.INSTANCE.calculate(bet);
+		var result = calculateCapture(bet);
 
-		assertThat(result.getMaxReturn()).isEqualByComparingTo(new Decimal("20"));
+		assertThat(result.getMaxReturn()).isEqualTo("20.0");
 	}
 
 	@Test
 	void calculateAccumulatorBet() {
-		var bet = new Bet(Accumulator.INSTANCE, "2", List.of(
-			new BetLeg("2.0", LegStatus.Companion.getOPEN(), IRDescriptor.Companion.getNO_IR(), false),
-			new BetLeg("3.0", LegStatus.Companion.getOPEN(), IRDescriptor.Companion.getNO_IR(), false)
-		), List.of(), BetRules.Companion.getDEFAULT());
+		var bet = new Bet("Accumulator", "2", new BetLeg[] {
+			new BetLeg("2.0"),
+			new BetLeg("3.0")
+		});
 
-		var result = BetCaptureCalculator.INSTANCE.calculate(bet);
+		var result = calculateCapture(bet);
 
-		assertThat(result.getMaxReturn()).isEqualByComparingTo(new Decimal("12"));
+		assertThat(result.getMaxReturn()).isEqualTo("12.00");
 	}
 
 	@Test
 	void calculateDoublesBet() {
-		var bet = new Bet(Doubles.INSTANCE, "1", List.of(
-			new BetLeg("2.0", LegStatus.Companion.getOPEN(), IRDescriptor.Companion.getNO_IR(), false),
-			new BetLeg("3.0", LegStatus.Companion.getOPEN(), IRDescriptor.Companion.getNO_IR(), false),
-			new BetLeg("4.0", LegStatus.Companion.getOPEN(), IRDescriptor.Companion.getNO_IR(), false)
-		), List.of(), BetRules.Companion.getDEFAULT());
+		var bet = new Bet("Doubles", "1", new BetLeg[] {
+			new BetLeg("2.0"),
+			new BetLeg("3.0"),
+			new BetLeg("4.0")
+		});
 
-		var result = BetCaptureCalculator.INSTANCE.calculate(bet);
+		var result = calculateCapture(bet);
 
-		assertThat(result.getMaxReturn()).isEqualByComparingTo(new Decimal("26"));
+		assertThat(result.getMaxReturn()).isEqualTo("26.00");
 	}
 
 	@Test
 	void calculateBigPermsBet() {
-		var bet = new Bet(new Perms(10), "1", List.of(
-			new BetLeg("1.1", LegStatus.Companion.getOPEN(), IRDescriptor.Companion.getNO_IR(), false),
-			new BetLeg("1.2", LegStatus.Companion.getOPEN(), IRDescriptor.Companion.getNO_IR(), false),
-			new BetLeg("1.4", LegStatus.Companion.getOPEN(), IRDescriptor.Companion.getNO_IR(), false),
-			new BetLeg("1.6", LegStatus.Companion.getOPEN(), IRDescriptor.Companion.getNO_IR(), false),
-			new BetLeg("1.8", LegStatus.Companion.getOPEN(), IRDescriptor.Companion.getNO_IR(), false),
-			new BetLeg("2.0", LegStatus.Companion.getOPEN(), IRDescriptor.Companion.getNO_IR(), false),
-			new BetLeg("3.0", LegStatus.Companion.getOPEN(), IRDescriptor.Companion.getNO_IR(), false),
-			new BetLeg("4.0", LegStatus.Companion.getOPEN(), IRDescriptor.Companion.getNO_IR(), false),
-			new BetLeg("5.0", LegStatus.Companion.getOPEN(), IRDescriptor.Companion.getNO_IR(), false),
-			new BetLeg("6.0", LegStatus.Companion.getOPEN(), IRDescriptor.Companion.getNO_IR(), false),
-			new BetLeg("1.1", LegStatus.Companion.getOPEN(), IRDescriptor.Companion.getNO_IR(), false),
-			new BetLeg("1.2", LegStatus.Companion.getOPEN(), IRDescriptor.Companion.getNO_IR(), false),
-			new BetLeg("1.4", LegStatus.Companion.getOPEN(), IRDescriptor.Companion.getNO_IR(), false),
-			new BetLeg("1.6", LegStatus.Companion.getOPEN(), IRDescriptor.Companion.getNO_IR(), false),
-			new BetLeg("1.8", LegStatus.Companion.getOPEN(), IRDescriptor.Companion.getNO_IR(), false),
-			new BetLeg("2.0", LegStatus.Companion.getOPEN(), IRDescriptor.Companion.getNO_IR(), false),
-			new BetLeg("3.0", LegStatus.Companion.getOPEN(), IRDescriptor.Companion.getNO_IR(), false),
-			new BetLeg("4.0", LegStatus.Companion.getOPEN(), IRDescriptor.Companion.getNO_IR(), false),
-			new BetLeg("5.0", LegStatus.Companion.getOPEN(), IRDescriptor.Companion.getNO_IR(), false),
-			new BetLeg("6.0", LegStatus.Companion.getOPEN(), IRDescriptor.Companion.getNO_IR(), false)
-		), List.of(), BetRules.Companion.getDEFAULT());
+		var bet = new Bet("Perms10", "1", new BetLeg[] {
+			new BetLeg("1.1"),
+			new BetLeg("1.2"),
+			new BetLeg("1.4"),
+			new BetLeg("1.6"),
+			new BetLeg("1.8"),
+			new BetLeg("2.0"),
+			new BetLeg("3.0"),
+			new BetLeg("4.0"),
+			new BetLeg("5.0"),
+			new BetLeg("6.0"),
+			new BetLeg("1.1"),
+			new BetLeg("1.2"),
+			new BetLeg("1.4"),
+			new BetLeg("1.6"),
+			new BetLeg("1.8"),
+			new BetLeg("2.0"),
+			new BetLeg("3.0"),
+			new BetLeg("4.0"),
+			new BetLeg("5.0"),
+			new BetLeg("6.0")
+		});
 
-		var result = BetCaptureCalculator.INSTANCE.calculate(bet);
+		var result = calculateCapture(bet);
 
-		assertThat(result.getMaxReturn()).isEqualByComparingTo(new Decimal("1662209157.7375774976"));
+		assertThat(result.getMaxReturn()).isEqualTo("1662209157.7375774976");
 	}
 }
